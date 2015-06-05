d1fb74c9972da9b0201208afd3b6dcc086f93434
lingshuo <lingshuo1993@hotmail.com>
2015-06-05 17:34:39 +0800

    modify page

--- src/cn/lisa/smartventilator/fragment/MonitorFragment.java
+++ src/cn/lisa/smartventilator/fragment/MonitorFragment.java
@@ -5,22 +5,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import cn.lisa.smartventilator.R;
 
 public class MonitorFragment extends Fragment{
+	private ToggleButton mTogBtn;
+	private TextView tv_smoke;
+	private TextView tv_phenolic;
+	private TextView tv_pm2_5;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_monitor, null);
-		TextView tv1=(TextView)view.findViewById(R.id.monitor_smoke_result);
-		TextView tv2=(TextView)view.findViewById(R.id.monitor_phenolic_result);
-		TextView tv3=(TextView)view.findViewById(R.id.monitor_pm2_5_result);		
-		tv1.setText("良好");
-		tv2.setText("良好");
-		tv3.setText("良好");
-		
+		tv_smoke=(TextView)view.findViewById(R.id.monitor_smoke_result);
+		tv_phenolic=(TextView)view.findViewById(R.id.monitor_phenolic_result);
+		tv_pm2_5=(TextView)view.findViewById(R.id.monitor_pm2_5_result);		
+		tv_smoke.setText("良好");
+		tv_phenolic.setText("良好");
+		tv_pm2_5.setText("良好");
+		mTogBtn = (ToggleButton) view.findViewById(R.id.mTogBtn); // 获取到控件
+		mTogBtn.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if(isChecked){
+					//选中
+				}else{
+					//未选中
+				}
+			}
+		});// 添加监听事件
 		return view;
 	}
 
