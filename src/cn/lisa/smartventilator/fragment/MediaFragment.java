2bfadf4fa3a7659ce41fcc61d8eab8e3742f8c3b
lingshuo <lingshuo1993@hotmail.com>
2015-06-08 18:14:30 +0800

    today

--- src/cn/lisa/smartventilator/fragment/MediaFragment.java
+++ src/cn/lisa/smartventilator/fragment/MediaFragment.java
@@ -2,23 +2,46 @@ package cn.lisa.smartventilator.fragment;
 
 import cn.lisa.smartventilator.R;
 import android.app.Fragment;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class MediaFragment extends Fragment{
+public class MediaFragment extends Fragment {
+	private ImageView mPlayBtn;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_media, null);
+		mPlayBtn = (ImageView) view.findViewById(R.id.media_play_button);
+		Context context = getActivity();
+		final PackageManager packageManager = context.getPackageManager();
+		mPlayBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = packageManager
+						.getLaunchIntentForPackage("com.youku.phone");
+				if (intent == null) {
+					System.out.println("APP not found!");
+				} else {
+					startActivity(intent);
+				}
+			}
+		});
 		return view;
 	}
 
 	private ImageView imageView;
-	
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
